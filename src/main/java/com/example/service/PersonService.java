a471b45829a3f47487a876a2d602c8a3dbfa376c
salah-eddine <salma.1994.199921@gmail.com>
2023-08-14 20:51:59 +0200

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,12 @@ public class PersonService {
 		p.setFirstName("test");
 		p.setLastName("test");
 		persons.add(p);
+		p = new Person();
+		p.setId("48");
+		p.setAge(21);
+		p.setFirstName("test09");
+		p.setLastName("test09");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
